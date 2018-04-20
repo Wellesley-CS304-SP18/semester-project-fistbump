343ce8174d16769b62e03d4426c16bea7f1628ee
Lauren  Luo class of 2020 <lluo2@tempest.wellesley.edu>
2018-04-20 15:14:08 -0400

    script to insert test data works now

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -8,9 +8,9 @@ INSERT INTO department (deptName, city, university) VALUES ('CS', 'Wellesley', '
 INSERT INTO department (deptName, city, university) VALUES ('Math', 'Northampton', 'Smith College');
 INSERT INTO department (deptName, city, university) VALUES ('Physics', 'Providence', 'Brown University');
 
-INSERT INTO reu_opp(deptID, link, classPref, deadline, isUROP, poster) VALUES (1, 'cs.wellesley.edu', 'sophomore', 2018-04-18, true, 1);
-INSERT INTO reu_opp(deptID, link, classPref, deadline, isUROP, poster) VALUES (2, 'math.smith.edu', 'upperclassman', 2018-04-18, false, 2);
-INSERT INTO reu_opp(deptID, link, classPref, deadline, isUROP, poster) VALUES (3, 'physics.brown.edu', 'underclassman', 2018-04-18, false, 1);
+INSERT INTO reu_opp(deptID, link, classPref, deadline, isUROP, poster) VALUES (1, 'cs.wellesley.edu', 'sophomore', '2018-04-18', true, 1);
+INSERT INTO reu_opp(deptID, link, classPref, deadline, isUROP, poster) VALUES (2, 'math.smith.edu', 'upperclassman', '2018-04-18', false, 2);
+INSERT INTO reu_opp(deptID, link, classPref, deadline, isUROP, poster) VALUES (3, 'physics.brown.edu', 'underclassman', '2018-04-18', false, 1);
 
 INSERT INTO reu_review(deptID, reviewer, review) VALUES (2, 1, 'WOW SMITH IS AWESOME');
 INSERT INTO reu_review(deptID, reviewer, review) VALUES (1, 3, 'Yeah this place is kinda sketchy');
@@ -24,6 +24,10 @@ INSERT into human_resources (uID, uName, cID, email, personType, poster) VALUES 
 INSERT into human_resources (uID, uName, cID, email, personType, poster) VALUES (2, 'Marissa', 1, 'mshen4@wellesley.edu', 'referral', 2 );
 INSERT into human_resources (uID, uName, cID, email, personType, poster) VALUES (3, 'Lauren', 1, 'lluo2@wellesley.edu', 'referral', 3 );
 
+INSERT into job_opp(cID, link, classPref, jobTitle, jobType, season, deadline, poster) VALUES (1, 'google.com', 'freshman', 'ENGINEERING', 'internship', 'summer', '2019-01-01', 1);
+INSERT into job_opp(cID, link, classPref, jobTitle, jobType, season, deadline, poster) VALUES (2, 'google.com', 'freshman', 'ENGINEERING', 'internship', 'summer', '2019-01-01', 2);
+INSERT into job_opp(cID, link, classPref, jobTitle, jobType, season, deadline, poster) VALUES (3, 'google.com', 'freshman', 'ENGINEERING', 'internship', 'summer', '2019-01-01', 3);
+
 INSERT into job_location(jobID, city) VALUES (1, 'Boston, MA');
 INSERT into job_location(jobID, city) VALUES (1, 'San Francisco, CA');
 INSERT into job_location(jobID, city) VALUES (1, 'Seattle, WA');
@@ -31,8 +35,3 @@ INSERT into job_location(jobID, city) VALUES (1, 'Seattle, WA');
 INSERT into job_review(jobID, jobYear, reviewer, review) VALUES (1, 2018, 1, 'This place is great!');
 INSERT into job_review(jobID, jobYear, reviewer, review) VALUES (1, 2018, 2, 'This place is OK...');
 INSERT into job_review(jobID, jobYear, reviewer, review) VALUES (1, 2018, 3, 'This place is bad.');
-
-INSERT into job_opp(jobID, cID, link, classPref, jobTitle, jobType, season, deadline, poster) VALUES (1, 1, 'google.com', 'freshman', 'ENGINEERING', 'internship', 'summer', 2019-01-01, 1);
-INSERT into job_opp(jobID, cID, link, classPref, jobTitle, jobType, season, deadline, poster) VALUES (1, 2, 'google.com', 'freshman', 'ENGINEERING', 'internship', 'summer', 2019-01-01, 2);
-INSERT into job_opp(jobID, cID, link, classPref, jobTitle, jobType, season, deadline, poster) VALUES (1, 3, 'google.com', 'freshman', 'ENGINEERING', 'internship', 'summer', 2019-01-01, 3);
-
