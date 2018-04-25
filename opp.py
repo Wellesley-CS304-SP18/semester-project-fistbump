635e1d6e5071c25edbf380e1bb3497d4a186575b
marissashen <mshen4@wellesley.edu>
2018-04-25 01:34:54 -0400

    update opp.py & search.py
    opp.py:
    - update & add human resource
    - check person (email) is user, add new user
    
    search.py:
    - return all reus
    - framework for searching specific jobs & reus

--- opp.py
+++ opp.py
@@ -31,23 +31,23 @@ def isAdmin(conn, uID):
     return info['uType'] == "admin"
 
 # update job opp
-def updateJob(conn, jobID, uID, cID, link, classPref, jobType, jobTitle,
-              positionName, season, deadline, companyName):
+def updateJob(conn, jobID, uID, companyName, link, classPref, jobType, jobTitle,
+              positionName, season, deadline):
     if canUpdate(conn, "job", jobID, uID):
         curs = conn.cursor(MySQLdb.cursors.DictCursor)
         curs.execute('start transaction')
-        curs.execute('update job_opp set poster=%s, cID=%s, link=%s, \
-            classPref=%s, jobType=%s, jobTitle=%s, positonName=%s, season=%s, \
+        curs.execute('update job_opp set poster=%s, link=%s, classPref=%s, \
+            jobType=%s, jobTitle=%s, positonName=%s, season=%s, \
             deadline=%s, companyName=%s where jobID=%s',
-            [uID, cID, link, classPref, jobType, jobTitle, positonName,
+            [uID, link, classPref, jobType, jobTitle, positonName,
             season, deadline, companyName, jobID])
         curs.execute('commit')
-        return "Updated job opportunity to: poster="+poster+"; cID="+cID+"; \
+        return "Updated job opportunity to: poster="+uID+"; cID="+cID+"; \
             link="+link+"; classPref="+classPref+"; jobType="+jobType+"; \
             postionName="+positionName+"; season="+season+"; deadline="+
             deadline+"; companyName="+companyName"."
-    return "Cannot update job opportunity. You are not the original job poster \
-        or an admin."
+    return "Cannot update job opportunity. You are not the original poster or \
+        an admin."
 
 # check if company name is in company table & add it if it isn't
 def checkCompany(conn, companyName):
@@ -82,7 +82,7 @@ def addJob(conn, uID, companyName, link, classPref, jobType, jobTitle,
     curs.execute('insert into location (jobID, city) values (%s, %s)',
         [jobID, location])
     curs.execute('commit')
-    return "Added new job opportunity: poster="+poster+"; companyName="+
+    return "Added new job opportunity: poster="+uID+"; companyName="+
         companyName+"; link="+link+"; classPref="+classPref+"; jobType="+
         jobType+"; postionName="+positionName+"; season="+season+"; deadline="+
         deadline+"."
@@ -95,13 +95,13 @@ def updateREU(conn, reuID, uID, deptID, link, classPref, deadline, isUROP):
             curs.execute('update reu_opp set poster=%s, deptID=%s, link=%s, \
                 classPref=%s, deadline=%s, isUROP=%s where reuID=%s',
                 [uID, deptID, link, classPref, deadline, isUROP, reuID])
-            return "Updated REU opportunity to: poster="+poster+"; deptID="+
+            return "Updated REU opportunity to: poster="+uID+"; deptID="+
                 deptID+"; link="+link+"; classPref="+classPref+"; deadline="+
                 deadline+"; isUROP="+isUROP+"."
         except:
             return "Wasn't able to update REU "+reuID+"."
-    return "Cannot update REU opportunity. You are not the original REU poster \
-        or an admin."
+    return "Cannot update REU opportunity. You are not the original poster or \
+        an admin."
 
 # check if dept is in department table & add it if it isn't; return deptID
 def addDepartment(conn, deptName, city, university):
@@ -126,14 +126,48 @@ def addREU(conn, uID, reuTitle, deptID, link, classPref, deadline, isUROP):
     curs.execute('insert into reu_opp (poster, reuTitle, deptID, link, \
         classPref, deadline, isUROP) values (%s, %s, %s, %s, %s, %s)',
         [uID, reuTitle, deptID, link, classPref, deadline, isUROP])
-    return "Added new REU opportunity: poster="+poster+"; reuTitle="+
+    return "Added new REU opportunity: poster="+uID+"; reuTitle="+
         reuTitle+"; deptID="+deptID+"; link="+link+"; classPref="+classPref+
         "; deadline="+deadline+"; isUROP="+isUROP+"."
 
 # update human resource
 def updateHR(conn, uID, hrUID, uName, companyName, email, personType):
     if canUpdate(conn, "hr", hrUID, uID):
-        
+        curs = conn.cursor(MySQLdb.cursors.DictCursor)
+        curs.execute('update human_resources set uName=%s, companyName=%s, \
+            email=%s, personType=%s, poster=%s, where uID=%s',
+            [uName, companyName, email, personType, uID, hrUID])
+        return "Updated human resource to: poster="+uID+"; uName="+uName+
+            "; companyName="+companyName+"; email="+email+"; personType="+
+            personType+"."
+    return "Cannot update human resource. You are not the original poster or \
+        an admin."
+
+# check if email is user & add person if isn't; return uID
+def checkUser(conn, uName, email):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute('start transaction')
+    curs.execute('select * from user_id where email=%s', [email])
+    info = curs.fetchone()
+    if info is None:
+        curs.execute('insert into user_id (uName, email, uType) \
+            values (%s, %s, %s)', [uName, email, 'general'])
+        curs.execute('select last_insert_id()')
+        info = curs.fetchone()
+        curs.execute('commit')
+        return info['last_insert_id()']
+    curs.execute('commit')
+    return info['uID']
+
+# add human resource
+def addHR(conn, uID, hrUID, uName, companyName, email, personType):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute('insert into human_resources (poster, uID, uName, \
+        companyName, personType, email) values (%s, %s, %s, %s, %s, %s)',
+        [uID, hrUID, uName, companyName, personType, email])
+    return "Added new human resource: poster="+uID+"; uID="+hrUID+"; uName="+
+        uName+"; companyName="+companyName+"; personType="+personType+
+        "; email="+email+"."
 
 # ==============================================================================
 
