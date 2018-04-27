7df60ec0de9c9dcff5e7326e30fd20285130b1cd
Lauren  Luo class of 2020 <lluo2@tempest.wellesley.edu>
2018-04-27 00:40:37 -0400

    login route fully functional

--- login.py
+++ login.py
@@ -8,14 +8,14 @@ import dbconn2
 
 def getPwd(conn, email):
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
-    curs.execute("SELECT pwd FROM user_id WHERE email=%s", [uName])
+    curs.execute("SELECT pwd FROM user_id WHERE email=%s", [email])
     return curs.fetchone()
 
 def emailIsFree(conn, email):
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
-    curs.execute("SELECT uID FROM user_id WHERE email=%s", [uName])
+    curs.execute("SELECT uID FROM user_id WHERE email=%s", [email])
     return curs.fetchone() == None
 
 def insertUser(conn, uName, email, pwd):
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
-    curs.execute("INSERT INTO user_id(uType, uName, email, pwd) VALUES (general, %s, %s, %s)", ([uName], [email], [pwd]))
+    curs.execute("INSERT INTO user_id(uType, uName, email, pwd) VALUES ('general', %s, %s, %s)", ([uName], [email], [pwd]))
