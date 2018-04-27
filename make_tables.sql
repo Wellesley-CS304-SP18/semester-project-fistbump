9d6449fca1e462a9d30de7bbb2a53f81b3f473ba
marissashen <mshen4@wellesley.edu>
2018-04-27 00:22:29 -0400

    updates
    make_tables.sql:
    - add city table & editted necessary referential issues (city so can easily search for opps via city)
    - add university table & editted necessary refeential issues (university so can easily search for reus via university)
    
    opp.py:
    - added more functions for updates/adds/deletes to tables

--- make_tables.sql
+++ make_tables.sql
@@ -8,21 +8,30 @@ DROP TABLE IF EXISTS company;
 DROP TABLE IF EXISTS reu_review;
 DROP TABLE IF EXISTS reu_opp;
 DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS university;
+DROP TABLE IF EXISTS city;
 DROP TABLE IF EXISTS user_id;
-	
+
 CREATE TABLE user_id (uID int AUTO_INCREMENT PRIMARY KEY,
-		      uType ENUM('general', 'admin') DEFAULT 'general',
+		                  uType ENUM('general', 'admin') DEFAULT 'general',
                       uName varchar(100) NOT NULL,
                       email varchar(100) NOT NULL,
                       pwd varchar(255)) ENGINE=InnoDB;
 
+CREATE TABLE city (cID int NOT NULL AUTO_INCREMENT PRIORITY KEY,
+                   city varchar(50) NOT NULL) ENGINE=InnoDB;
+
+CREATE TABLE university (university varchar(50) NOT NULL PRIORITY KEY) ENGINE=InnoDB;
+
 CREATE TABLE department (deptID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
                          deptName varchar(100) NOT NULL,
-                         city varchar(50) NOT NULL,
-                         university varchar(50) NOT NULL) ENGINE=InnoDB;
+                         cID int NOT NULL,
+                         university int NOT NULL,
+											   FOREIGN KEY (cID) REFERENCES city(cID) ON DELETE RESTRICT,
+											   FOREIGN KEY (university) REFERENCES university(university) ON DELETE CASCADE) ENGINE=InnoDB;
 
 CREATE TABLE reu_opp (reuID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-		      reuTitle varchar(100) NOT NULL,
+		                  reuTitle varchar(100) NOT NULL,
                       deptID int NOT NULL,
                       link varchar(255) NOT NULL,
                       classPref ENUM('freshman', 'sophomore', 'junior', 'senior', 'underclassman', 'upperclassman', 'all') NOT NULL,
@@ -45,13 +54,13 @@ CREATE TABLE job_opp (jobID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
                       classPref ENUM('freshman', 'sophomore', 'junior', 'senior', 'underclassman', 'upperclassman', 'all') NOT NULL,
                       jobTitle ENUM('ENGINEERING', 'DESIGN', 'PM', 'OTHER') NOT NULL,
                       jobType ENUM('internship', 'part-time', 'full-time') NOT NULL,
-		      positionName varchar(50) NOT NULL,
+		      						positionName varchar(50) NOT NULL,
                       season ENUM('fall', 'spring', 'summer', 'winter', 'year-round') NOT NULL,
                       deadline DATE NOT NULL,
                       poster int NOT NULL,
-		      companyName varchar(100) NOT NULL, 
+		      						companyName varchar(100) NOT NULL,
                       FOREIGN KEY (poster) REFERENCES user_id(uID) ON DELETE RESTRICT,
-		      FOREIGN KEY (companyName) REFERENCES company(companyName) ON DELETE RESTRICT) ENGINE=InnoDB;
+		      						FOREIGN KEY (companyName) REFERENCES company(companyName) ON DELETE RESTRICT) ENGINE=InnoDB;
 
 CREATE TABLE job_review (jobID int,
                          jobYear int NOT NULL,
@@ -61,16 +70,17 @@ CREATE TABLE job_review (jobID int,
                          FOREIGN KEY (reviewer) REFERENCES user_id(uID) ON DELETE CASCADE) ENGINE=InnoDB;
 
 CREATE TABLE job_location (jobID int NOT NULL,
-                           city varchar(50) NOT NULL,
+                           cID int NOT NULL,
                            PRIMARY KEY (jobID, city),
-                           FOREIGN KEY (jobID) REFERENCES job_opp(jobID)) ENGINE=InnoDB;
+                           FOREIGN KEY (jobID) REFERENCES job_opp(jobID) ON DELETE CASCADE,
+												   FOREIGN KEY (cID) REFERENCES city(cID) ON DELETE RESTRICT) ENGINE=InnoDB;
 
 CREATE TABLE human_resources(uID int NOT NULL,
-       	     		     uName varchar(100) NOT NULL,
-			     companyName int NOT NULL,
-			     email varchar(100) NOT NULL,
-			     personType ENUM('recruiter', 'referral') NOT NULL,
-			     poster int NOT NULL,
-			     FOREIGN KEY (uID) REFERENCES user_id(uID) ON DELETE CASCADE,
-			     FOREIGN KEY (companyName) REFERENCES company(companyName) ON DELETE CASCADE,
-			     FOREIGN KEY (poster) REFERENCES user_id(uID) ON DELETE RESTRICT) ENGINE=InnoDB; 
+       	     		             uName varchar(100) NOT NULL,
+			     									 companyName int NOT NULL,
+			     									 email varchar(100) NOT NULL,
+			     									 personType ENUM('recruiter', 'referral') NOT NULL,
+			     									 poster int NOT NULL,
+			     									 FOREIGN KEY (uID) REFERENCES user_id(uID) ON DELETE CASCADE,
+			     									 FOREIGN KEY (companyName) REFERENCES company(companyName) ON DELETE CASCADE,
+			     									 FOREIGN KEY (poster) REFERENCES user_id(uID) ON DELETE RESTRICT) ENGINE=InnoDB;
