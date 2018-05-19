04d687f35585f623ad4812562fe7263f0b264298
HavannahTran <htran@wellesley.edu>
2018-05-19 02:35:50 -0700

    small rename

--- static/functions.js
+++ static/functions.js
@@ -1,6 +1,6 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction() {
+function dropDown() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
